6a89b3f27315b04ca97fb294928fdc3fde4aa32c
Ranjeet <kendreranjet07@gmail.com>
2022-06-06 18:13:04 +0530

    First Commit

---
+++ src/Demo/Test.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++) {   // ROw
+			
+		for(int j=1;j<=5;j++) {   // Columns
+			
+          System.out.print("*");
+    
+		}     
+		System.out.println();
+	}
+	}
+
+}
